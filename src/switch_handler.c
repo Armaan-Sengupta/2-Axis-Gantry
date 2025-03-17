1d12c8a0a092207dd7dabd892f3f26783c10f392
Armaan Sengupta <asengupt@uwaterloo.ca>
2025-03-17 13:58:28 -0400

    Added logic to stop motors

--- src/switch_handler.c
+++ src/switch_handler.c
@@ -2,6 +2,12 @@
 
 const uint32_t debounce_delay_loops = 50000;
 
+void init_switch_directions(){
+    permited_directions[MOTOR_X_FORWARD] = GPIO_Read(GPIOA, GPIO_PIN_10);
+    permited_directions[MOTOR_X_BACKWARD] = GPIO_Read(GPIOA, GPIO_PIN_8);
+    permited_directions[MOTOR_Y_FORWARD] = GPIO_Read(GPIOA, GPIO_PIN_9);
+    permited_directions[MOTOR_Y_BACKWARD] = GPIO_Read(GPIOB, GPIO_PIN_4);;
+}
 
 void handle(uint8_t pin)
 {    
@@ -13,24 +19,34 @@ void handle(uint8_t pin)
     case 10:
         switch_state = GPIO_Read(GPIOA, GPIO_PIN_10);
         permited_directions[MOTOR_X_FORWARD] = switch_state;
+        if (switch_state == false){
+            stop_motor(MOTOR_X);
+        }
         break;
     case 8:
         switch_state = GPIO_Read(GPIOA, GPIO_PIN_8);
         permited_directions[MOTOR_X_BACKWARD] = switch_state;
+        if (switch_state == false){
+            stop_motor(MOTOR_X);
+        }
         break;
     case 9:
         switch_state = GPIO_Read(GPIOA, GPIO_PIN_9);
         permited_directions[MOTOR_Y_FORWARD] = switch_state;
+        if (switch_state == false){
+            stop_motor(MOTOR_Y);
+        }
         break;
     case 4:
         switch_state = GPIO_Read(GPIOB, GPIO_PIN_4);
         permited_directions[MOTOR_Y_BACKWARD] = switch_state;
+        if (switch_state == false){
+            stop_motor(MOTOR_Y);
+        }
         break;
     default:
         break;
     }
 
-    if (switch_state == false){
-        stop_motor(MOTOR_X);
-    }
+
 }
